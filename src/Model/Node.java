ac8b9e1ec42b27e007e848727802c97ebe8ab5ad
Jascha <jascha.coetser@my.jcu.edu.au>
2019-11-01 22:04:06 +1000

    Incorporated node class that works along side the road class

---
+++ src/Model/Node.java
@@ -0,0 +1,45 @@
+package Model;
+
+import java.util.ArrayList;
+
+//This class works along side my road class and is what implements each section of the road
+public class Node {
+//    ArrayList<Node> nextNodes = new ArrayList<>();
+//    ArrayList<Node> previousNodes = new ArrayList<>();
+//    Vehicle vehicle;
+//    boolean canGo = true;
+//    private TrafficLights trafficLights;
+//
+//
+////    public Road(Road previous_road, Road next_road) {
+////        this.previous_road = previous_road;
+////        this.next_road = next_road;
+////    }
+//
+//
+//
+//
+//    public Vehicle getVehicle() {
+//        return vehicle;
+//    }
+//
+//    public void setVehicle(Vehicle vehicle) {
+//        this.vehicle = vehicle;
+//    }
+//
+//    public boolean isCanGo() {
+//        return canGo;
+//    }
+//
+//    public void setCanGo(boolean canGo) {
+//        this.canGo = canGo;
+//    }
+//
+//    public TrafficLights getTrafficLights() {
+//        return trafficLights;
+//    }
+//
+//    public void setTrafficLights(TrafficLights trafficLights) {
+//        this.trafficLights = trafficLights;
+//    }
+}
